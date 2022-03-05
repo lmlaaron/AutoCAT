73aca34dc4d8171f0a5fdd1164040ace997debe3
wenjiex <wenjiex@vt.edu>
2022-03-04 20:46:23 -0500

    rename addresses

--- src/categorization_parser.py
+++ src/categorization_parser.py
@@ -4,13 +4,13 @@ from cache_guessing_game_env_impl import CacheGuessingGameEnv
 from pandas.core.arrays import numeric
 
 class CategorizationParser:
-  def __init__(self,filename):
-    self.filename = filename
+  def __init__(self):
+    self.attacker_address_range_max = 8 #not include, the max value of addresses
     self.number_of_set = 2
 
-  def readfile(self):# python categorization_parser.py temp.txt
+  def readfile(self,filename):# python categorization_parser.py temp.txt
     patterns=[]
-    f = open(self.filename, mode='r', encoding='UTF-8')
+    f = open(filename, mode='r', encoding='UTF-8')
     lines = f.readlines()
     for d in lines:
       d = d.split()
@@ -29,71 +29,70 @@ class CategorizationParser:
     df.columns =['addr', 'is_guess', 'is_victim', 'is_flush', 'victim_addr']
     return df
 
-  def get_set(self, row): #return addr%self.number_of_set
+  def _get_set(self, row): #return addr%self.number_of_set
     
     #df.addr = df.addr%self.number_of_set
     return row['addr']%self.number_of_set
 
-  def add_setcolumn(self, df):
+  def add_set_column(self, df):
     #df['set'] = df.apply(self, get_set(df.addr)) 
-    df['set'] = df.apply (lambda row: self.get_set(row), axis=1)
+    df['set'] = df.apply (lambda row: self._get_set(row), axis=1)
     return df
     
+  def _make_order(self, df, col_name):
+    """return the order of each element in df[col_name]"""
+    order = [-1 for i in range(self.attacker_address_range_max)] # could be further optimzed if min address is not 0
+    cnt = 0
+    for index, row in df.iterrows():
+      value = row[col_name]
+      if order[value] == -1:
+        order[value] = cnt
+        cnt = cnt + 1
+    print(f'order = {order}')
+    return order
 
-  def get_order(self, df): #return order_array
-    total_rows = df.count
-    for index in range(total_rows-1, 0, -1):
-    print(df2)
+  def _get_order(self, row, col_name, order): 
+    """return the order of each element in df[col_name]"""
+    return order[row[col_name]]
 
-  def rename_column(self,df):
-    #set0 = df[df.set==0]
-    #set0['order'] = set0['addr'].rank(method='dense',ascending=False).astype(int)
-    #set1 = df[df.set==1]
-    #set1['order'] = set1['addr'].rank(method='dense',ascending=False).astype(int)
-    df['order'] = df['addr'].rank(method='min',ascending=False).astype(int)
-    #frames = [set0, set1]
-    #result = pd.concat(frames)
-    #output = pd.DataFrame(result)
-    #output =df.sort_index(axis=0, ascending=True)
+  def rename_column(self,df, col_name):
+    """rename the column based on the order the item appear in the column"""
+    order = self._make_order(df, col_name)
+    new_col_name = col_name + '_renamed'
+    df[new_col_name] = df.apply (lambda row: self._get_order(row, col_name, order), axis=1)
     return df
 
-  def remove_rep(self, pattern): # return pattern.drop_duplicates()
-    pass
+  def remove_rep(self, df): # return pattern.drop_duplicates()
+    return df.drop_duplicates()
 
   def main_parser(self, pattern):
-    #pattern_v = []
-    #for action in pattern:
-    #  action_verbose = env.parse_action(action)
-    #  pattern_v.append(action_verbose)
-    #pattern_v = self.rename_address(pattern_v)
-    #pattern_v = self.remove_rep(pattern_v)
-    #return pattern_v
-    pass
+    """output a pattern after renaming,
+    format [is_guess,  is_victim,  is_flush,  victim_addr,  addr_renamed,  set_renamed]"""
+    pattern_parsed = []
+    for action in pattern :
+      action_parsed = self.parse_action(action)
+      pattern_parsed.append(action_parsed)
+    df = self.convert_dataframe(pattern_parsed)
+    df = self.add_set_column(df)
+    df = self.rename_column(df, 'addr') # rename address
+    df = self.rename_column(df, 'set') # rename set
+    df = self.remove_rep(df) #remove repeated action
+    df = df.drop(columns=['addr', 'set'], axis=1)
+    output = df.values.tolist()
+    return output
 
 def main(argv): # Defining main function
   filename = argv[1]
   print(filename)
 
-  categorization_parser = CategorizationParser(filename)
-  patterns = categorization_parser.readfile()
+  categorization_parser = CategorizationParser()
+  patterns = categorization_parser.readfile(filename)
   print(patterns)
   
-  for pattern in patterns :
-    pattern_parsed = []
-    for action in pattern :
-      action_parsed = categorization_parser.parse_action(action)
-      pattern_parsed.append(action_parsed)
-    print(pattern_parsed)
+  base_pattern = categorization_parser.main_parser(patterns[0])
+  #for pattern in patterns :
+  #  base_pattern = categorization_parser.main_parser(pattern)
   
-  #pattern_parsed = categorization_parser.parse_action(action)
-
-  df = categorization_parser.convert_dataframe(pattern_parsed)
-  df = categorization_parser.add_setcolumn(df)
-  print(df)
-  df = categorization_parser.get_order(df)
-  #df = categorization_parser.rename_column(df)
-  
-  print(df)
 
 if __name__=="__main__": # Using the special variable
     main(sys.argv)
