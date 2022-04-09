4b92ca35590d542a6e705b49804e2c3709310812
Mulong Luo <ml2558@cornell.edu>
2022-04-09 14:35:07 -0400

    added logging for rlmeta, exported correctness rate for each epoch

--- src/rlmeta/train_ppo.py
+++ src/rlmeta/train_ppo.py
@@ -19,19 +19,22 @@ from rlmeta.core.model import wrap_downstream_model
 from rlmeta.core.replay_buffer import ReplayBuffer, make_remote_replay_buffer
 from rlmeta.core.server import Server, ServerList
 from rlmeta.core.callbacks import EpisodeCallbacks
+from rlmeta.core.types import Action, TimeStep
 
 
 # add guess correctness statistics during training
 class MyCallbacks(EpisodeCallbacks):
     def __init__(self):
-        super.__init__()
+        super().__init__()
 
     def on_episode_step(self, index: int, step: int, action: Action,
                         timestep: TimeStep) -> None:
         obs, reward, done, info = timestep
-        if info['']:
-            self._custom_metrics = {"correct_rate": 1}
-
+        if info['is_guess'] == True:
+            if info['guess_correct'] == True:
+                self._custom_metrics = {"correct_rate": 1}
+            elif info['guess_correct'] == False:
+                self._custom_metrics = {"correct_rate": 0}
 
 from cache_env_wrapper import CacheEnvWrapperFactory
 from cache_ppo_model import CachePPOModel
@@ -39,6 +42,7 @@ from cache_ppo_model import CachePPOModel
 
 @hydra.main(config_path="./config", config_name="ppo")
 def main(cfg):
+    my_callbacks = MyCallbacks()
     logging.info(hydra_utils.config_to_json(cfg))
 
     env_fac = CacheEnvWrapperFactory(cfg.env_config)
@@ -89,7 +93,8 @@ def main(cfg):
                           should_update=True,
                           num_rollouts=cfg.num_train_rollouts,
                           num_workers=cfg.num_train_workers,
-                          seed=cfg.train_seed)
+                          seed=cfg.train_seed,
+                          episode_callbacks = my_callbacks)
     e_loop = ParallelLoop(env_fac,
                           e_agent_fac,
                           e_ctrl,
@@ -97,7 +102,8 @@ def main(cfg):
                           should_update=False,
                           num_rollouts=cfg.num_eval_rollouts,
                           num_workers=cfg.num_eval_workers,
-                          seed=cfg.eval_seed)
+                          seed=cfg.eval_seed,
+                          episode_callbacks = my_callbacks)
     loops = LoopList([t_loop, e_loop])
 
     servers.start()
