5d492f0ec72f7bb6ed2f7668102eed5a91f9ab1b
geunbae <geunbae@vt.edu>
2022-08-17 12:30:47 -0400

    still debugging, this config gets error while run

--- src/rlmeta/agents/evict_reload_agent.py
+++ src/rlmeta/agents/evict_reload_agent.py
@@ -20,14 +20,14 @@ class EvictReloadAgent():
             flush_inst = env_config["flush_inst"] if "flush_inst" in env_config else False            
             self.allow_empty_victim_access = env_config["allow_empty_victim_access"] if "allow_empty_victim_access" in env_config else False
             
-            assert(self.num_ways == 1) # currently only support direct-map cache
-            assert(self.cache_size == 8) # assume the cache config is for 8 sets 
-            assert(flush_inst == False) # do not allow flush instruction
+            #assert(self.num_ways == 1) # currently only support direct-map cache
+            #assert(self.cache_size == 8) # assume the cache config is for 8 sets 
+            #assert(flush_inst == False) # do not allow flush instruction
             #assert((attacker_addr_e - attacker_addr_s ) == (2 * (victim_addr_e - victim_addr_s )+1)) # address space must be shared
             
-            assert( attacker_addr_s == victim_addr_s)
+            #assert( attacker_addr_s == victim_addr_s)
             #assert( ( attacker_addr_e + 1 == victim_addr_s )) # or ( victim_addr_e + 1 == attacker_addr_s ) )
-            assert(self.allow_empty_victim_access == False)
+            #assert(self.allow_empty_victim_access == False)
             #assert(attacker_addr_s == 0)
             #assert(attacker_addr_e == 15)
             #assert(victim_addr_s == 0)
@@ -90,7 +90,7 @@ class EvictReloadAgent():
         # is_guess = 1
         # victim_addr = action - ( len(self.attacker_address_space) + 1 + 1) --> victim addr = action - 18
         elif self.local_step == 2 * self.cache_size - (self.cache_size if self.no_prime else 0 ):# - 1 - 1: # do guess and terminate
-            #action = 2 * self.cache_size # default assume that last is hit
+            action = self.cache_size # default assume that last is hit
             for addr in range(1, len(self.lat)):
                 if self.lat[addr].int() == 0: # 0 for hit, 1 for miss
                     action =  addr + self.cache_size 
@@ -98,6 +98,7 @@ class EvictReloadAgent():
             self.local_step = 0 # reset the attacker 
             self.lat=[] # reset the attacker
             self.no_prime = True # reset the attacker
+            
             if action > self.cache_size:
                 action+=1
             return action, info
