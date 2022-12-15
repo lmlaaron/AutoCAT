91a950663f985c995c62704d8b86562e322b1b0e
Xiaomeng Yang <bit.yangxm@gmail.com>
2022-12-15 15:18:07 -0800

    Add samples for table8 and table9

--- src/rlmeta/data/table8/hpca_ae_exp_8_autocor_new/exp1/sample.sh
+++ src/rlmeta/data/table8/hpca_ae_exp_8_autocor_new/exp1/sample.sh
@@ -1,2 +1,1 @@
-python sample_cchunter.py checkpoint=ppo_agent-499.pth num_episodes=1000
-
+python sample_cchunter.py env_config=hpca_ae_exp_8_autocor checkpoint=ppo_agent-499.pth num_episodes=1000
--- src/rlmeta/data/table9/hpca_ae_exp_9_svm_new/exp2/sample.sh
+++ src/rlmeta/data/table9/hpca_ae_exp_9_svm_new/exp2/sample.sh
@@ -1,1 +1,1 @@
-python sample_cyclone.py checkpoint=ppo_agent-499.pth num_episodes=1000
+python sample_cyclone.py env_config=hpca_ae_exp_9_svm checkpoint=ppo_agent-499.pth num_episodes=1000
