7a0bbd152912a584c80b7748077792353e97ed8d
Jiaxun Cui <cuijiaxun_sjtu@163.com>
2023-02-11 21:07:18 -0600

    clean up lstm+mlp in autocat setting

--- src/rlmeta/macta/train/train_ppo_lstm.py
+++ src/rlmeta/macta/train/train_ppo_lstm.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import copy
 import logging
 import time
@@ -20,16 +22,17 @@ from rlmeta.core.replay_buffer import ReplayBuffer, make_remote_replay_buffer
 from rlmeta.core.server import Server, ServerList
 from rlmeta.core.callbacks import EpisodeCallbacks
 from rlmeta.core.types import Action, TimeStep
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from cache_env_wrapper import CacheEnvWrapperFactory
-from cache_ppo_lstm import CachePPOLSTMModel
+from env import CacheEnvWrapperFactory
+from model import CachePPOLSTMModel
 from metric_callbacks import MetricCallbacks
 
 from utils.wandb_logger import WandbLogger
 
-@hydra.main(config_path="./config", config_name="ppo_lstm")
+@hydra.main(config_path="../config", config_name="ppo_lstm")
 def main(cfg):
-    wandb_logger = WandbLogger(project="rl4cache", config=cfg)
+    #wandb_logger = WandbLogger(project="rl4cache", config=cfg)
     my_callbacks = MetricCallbacks()
     logging.info(hydra_utils.config_to_json(cfg))
 
@@ -114,7 +117,7 @@ def main(cfg):
                 stats.json(info, phase="Train", epoch=epoch, time=cur_time))
         train_stats = stats
         time.sleep(1)
-        
+
         stats = agent.eval(cfg.num_eval_episodes)
         cur_time = time.perf_counter() - start_time
         info = f"E Epoch {epoch}"
@@ -126,7 +129,7 @@ def main(cfg):
         eval_stats = stats
         time.sleep(1)
         
-        wandb_logger.log(train_stats, eval_stats)
+        #wandb_logger.log(train_stats, eval_stats)
 
         torch.save(train_model.state_dict(), f"ppo_agent-{epoch}.pth")
 
